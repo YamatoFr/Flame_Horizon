44bae5f5701b9667020bbf009a38f7fbd95604f2
kemuelos <72893403+kemuelos@users.noreply.github.com>
2020-10-26 09:43:38 -0400

    Add files via upload

---
+++ Flame Horizon - 0.4/Class.py
@@ -0,0 +1,51 @@
+import pygame as pg, random as rd
+pg.font.init()
+pg.init()
+
+
+class ElementGraphique:
+	def __init__(self, img, x, y): 
+		self.image = img
+		self.rect = self.image.get_rect()
+		self.rect.x, self.rect.y = x, y
+
+
+	def afficher(self, fenetre):
+		fenetre.blit(self.image, self.rect)
+
+
+
+class PERSO(ElementGraphique):
+	def __init__(self, img, x=100, y=325):
+		super(PERSO, self).__init__(img, x, y)
+		self.vitesse = 7
+		self.vie = 50
+
+
+	def déplacer(self, touches, largeur, hauteur):
+		if (touches[pg.K_RIGHT]) and self.rect.x<largeur - 900:
+			self.rect.x += self.vitesse
+
+		if (touches[pg.K_LEFT]) and self.rect.x>0:
+			self.rect.x -= self.vitesse
+
+		if (touches[pg.K_UP]) and self.rect.y>0:
+			self.rect.y -= self.vitesse 
+
+		if (touches[pg.K_DOWN]) and self.rect.y<hauteur - self.rect.h:
+			self.rect.y += self.vitesse
+
+			
+
+
+
+class METEORITE(ElementGraphique):
+	def __init__(self, img, x , y):
+		super(METEORITE, self).__init__(img, x, y)
+		self.damage = 10
+		self.vy = 7
+
+	def move(self, largeur, hauteur,ennemies):
+		self.rect.y += self.vy
+
+								
---
+++ Flame Horizon - 0.4/Dico.py
@@ -0,0 +1,38 @@
+import pygame as pg
+from random import randint
+from Class import *
+pg.font.init()
+pg.init()
+
+largeur, hauteur = 1300, 650
+fenetre=pg.display.set_mode((largeur,hauteur))
+font_1 = pg.font.Font("ecriture/STJEDISE.ttf",100)
+
+
+
+images = {
+    'fond1' : pg.image.load("image/bg/fond1.jpg").convert(),
+    'fond2': pg.image.load("image/bg/fond_2.jpg").convert(),
+    'vaisseau1' : pg.image.load("image/perso/perso_1.png").convert_alpha(),
+    'vaisseau2' : pg.image.load("image/perso/perso_2.png").convert_alpha(),
+    'vaisseau3' : pg.image.load("image/perso/perso_3.png").convert_alpha(),
+    'vaisseau4' : pg.image.load("image/perso/perso_4.png").convert_alpha(),
+    'vaisseau5' : pg.image.load("image/perso/perso_5.png").convert_alpha(),
+    'ennemie1' : pg.image.load("image/ennemies/astéroide.png").convert_alpha() 
+}
+
+
+
+ecriture ={
+	'play' : font_1.render("PLAY",1, (255,255,255)),
+	'quit' : font_1.render("exit",1, (255,255,255)),
+	'play_select' : font_1.render("PLAY",1, (255,0,0)),
+	'quit_select' : font_1.render("exit",1, (255,0,0))
+}
+
+
+
+
+def ajouter(hauteur, largeur, images, ennemies):
+    ennemies.append(METEORITE(images['ennemie1'], randint(0, largeur), randint(0, hauteur//hauteur)))
+
