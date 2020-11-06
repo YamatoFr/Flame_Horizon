16cab9eb154099668c506bdbcb25a7fd709fc6d4
YamatoFr <60272310+YamatoFr@users.noreply.github.com>
2020-11-06 09:02:34 -0400

    Add files via upload

---
+++ Flame Horizon/Class.py
@@ -0,0 +1,99 @@
+import pygame as pg, random as rd
+pg.font.init()
+pg.init()
+
+
+class ElementGraphique:
+	def __init__(self, fenetre, img, x, y): 
+		self.image = img
+		self.rect = self.image.get_rect()
+		self.rect.x =x
+		self.rect.y =y
+		self.fenetre = fenetre
+
+	def afficher(self):
+		self.fenetre.blit(self.image, self.rect)
+
+
+class ElementAnime(ElementGraphique):
+
+	def __init__(self, fenetre, images, x=0, y=0):
+		super().__init__(fenetre, images[0], x, y)
+		self.images = images
+		self.timer = 0 # timer pour l'animation
+		self.numAnim = 0 # numéro de l'image courante
+
+	def afficher():
+		self.timer += 1
+		if self.timer > 5:
+			self.timer = 0
+			self.numAnim += 1
+			if self.numAnim >= len(self.images):
+				self.numAnim = 0
+				self.image = self.images[self.numAnim]
+
+			super().afficher()
+
+
+class DeplacementLineaire(ElementGraphique):
+	"""docstring for DeplacementLineaire"""
+	def __init__(self, fenetre, img, x=0, y=0):
+		ElementGraphique.__init__(self, fenetre, img, x, y)
+		self.dx = -3
+		self.dy = 0
+	
+	def deplacer(self):
+		self.rect.x += self.dx
+		self.rect.y += self.dy
+
+
+class Perso(ElementGraphique):
+	def __init__(self, fenetre, img, x=100, y=325):
+		super().__init__(fenetre, img, x, y)
+		self.vitesse = 7
+		self.vie = 50
+
+
+	def deplacer(self, touches, largeur, hauteur):
+		if (touches[pg.K_RIGHT] or touches[pg.K_d]) and self.rect.x < largeur - self.rect.w:
+			self.rect.x += self.vitesse
+
+		if (touches[pg.K_LEFT] or touches[pg.K_a]) and self.rect.x > 0:
+			self.rect.x -= self.vitesse
+
+		if (touches[pg.K_UP] or touches[pg.K_w]) and self.rect.y > 0:
+			self.rect.y -= self.vitesse 
+
+		if (touches[pg.K_DOWN] or touches[pg.K_s]) and self.rect.y < hauteur - self.rect.h:
+			self.rect.y += self.vitesse
+
+class Ennemis(ElementAnime):
+	"""docstring for Ennemis"""
+	def __init__(self, fenetre, img, x, y):
+		super().__init__(fenetre, images, x, y)
+		self.vie = 20
+		self.vitesse = 5
+
+
+class Tirs(ElementAnime):
+		"""docstring for Tirs"""
+		def __init__(self, fenetre, images, x=0, y=0):
+			super().__init__(fenetre, images, x, y)
+			self.dx = 5
+			self.dy = 0
+
+		def deplacer(self):
+			self.rect.x += self.dx
+			self.rect.y += self.dy 
+				
+
+class Meteorite(ElementGraphique):
+	def __init__(self, fenetre, img, x , y):
+		super().__init__(fenetre, img, x, y)
+		self.damage = 10
+		self.vy = 7
+
+	def move(self, largeur, hauteur,ennemies):
+		self.rect.y += self.vy
+
+								
---
+++ Flame Horizon/Dico.py
@@ -0,0 +1,44 @@
+import pygame as pg
+import random
+from random import randint
+from Class import *
+
+largeur, hauteur = 1300, 650
+fenetre=pg.display.set_mode((largeur,hauteur))
+font_1 = pg.font.Font("ecriture/STJEDISE.TTF",100)
+
+
+
+images = {}
+images['fond_0']= pg.image.load("image/bg/fond_0.jpg").convert()
+images['fond_1'] = pg.image.load("image/bg/fond_1.jpg").convert()
+images['vaisseau1'] = pg.image.load("image/perso/perso_1.png").convert_alpha()
+images['vaisseau2'] = pg.image.load("image/perso/perso_2.png").convert_alpha()
+images['vaisseau3'] = pg.image.load("image/perso/perso_3.png").convert_alpha()
+images['vaisseau4'] = pg.image.load("image/perso/perso_4.png").convert_alpha()
+images['vaisseau5'] = pg.image.load("image/perso/perso_5.png").convert_alpha()
+images['astéroide'] = pg.image.load("image/ennemies/astéroide.png").convert_alpha()
+images['tribase1_nor'] = pg.image.load("image/ennemies/tribase1_nor.png").convert_alpha()
+images['tribase1_chr'] = pg.image.load("image/ennemies/tribase1_chr.png").convert_alpha()
+images['tribase2_nor'] = pg.image.load("image/ennemies/tribase2_nor.png").convert_alpha()
+images['tribase2_chr'] = pg.image.load("image/ennemies/tribase2_chr.png").convert_alpha()
+images['tribase3_nor'] = pg.image.load("image/ennemies/tribase3_nor.png").convert_alpha()
+images['tribase3_chr'] = pg.image.load("image/ennemies/tribase3_chr.png").convert_alpha()
+images['flame'] = []
+for i in range(4):
+	images['flame'].append(pg.image.load('image/tirs/flameBall_'+str(i)+'.png').convert_alpha())
+
+
+ecriture ={}
+ecriture['play'] = font_1.render("PLAY", 1, (255,255,255))
+ecriture['quit'] = font_1.render("exit", 1, (255,255,255))
+ecriture['play_select'] = font_1.render("PLAY", 1, (255,0,0))
+ecriture['quit_select'] = font_1.render("exit", 1, (255,0,0))
+
+def tir(tirs, images, compteur, fps, duree):
+	if compteur/fps % duree ==0:
+		tirs.append(Tirs(fenetre, images['flame'], random.randint(0, largeur), random.randint(0, hauteur)))
+		#L'aléatoire n'est ici que pour le test
+
+def ajouter(hauteur, largeur, images, ennemies):
+	ennemies.append(Meteorite(fenetre, images["astéroide"], randint(0, largeur), randint(0, hauteur//hauteur)))
